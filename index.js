26db879936cca367317adf03a7dc9d407f89ff46
Franklin John Zamora <jfzam@Franklins-MacBook-Pro.local>
2019-01-17 16:56:19 +0800

    basic setup

--- index.js
+++ index.js
@@ -11,3 +11,12 @@ app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'))
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.get('/webhook', (req, res) => {
+  if(req.query['hub.verify_token'] === 'jayzero_token') {
+    res.send(req.query['hub.challenge'])
+  }
+  else {
+    res.send('Wrong token!')
+  }
+})
