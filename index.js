6edb8a45a11bdd6adb5de75bc5d361d5ca6911b2
Franklin John Zamora <jfzam@Franklins-MacBook-Pro.local>
2019-01-17 17:35:31 +0800

    added post

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/webhook', (req, res) => {
+app.get('/webhook/', (req, res) => {
   if(req.query['hub.verify_token'] === 'jayzero_token') {
     res.send(req.query['hub.challenge'])
   }
@@ -20,3 +20,41 @@ app.get('/webhook', (req, res) => {
     res.send('Wrong token!')
   }
 })
+
+const token = "<EAAMt85OjL0oBAHTGez3cQ1ZCUtnTZBsc3RkP7J1JcEr3xJUGLs4RXHwzapYGp0S5kHML6s0ZCIdb5lmC4kloOgyi4AO5spgZA9iOm0E4ROwZBxhntwVgxhB1qZACGLtyk7vBkJtl4QQZCRZCEvQf83hfgsvSYLBIZA6BBBUBxUvazZAQZDZD";
+app.post('/webhook/', function(req, res) {
+    var messaging_events = req.body.entry[0].messaging;
+    for (var i = 0; i < messaging_events.length; i++) {
+        var event = req.body.entry[0].messaging[i];
+        var sender = event.sender.id;
+        if (event.message && event.message.text) {
+            var text = event.message.text;
+            sendTextMessage(sender, text + "!");
+        }
+    }
+    res.sendStatus(200);
+});
+function sendTextMessage(sender, text) {
+    var messageData = {
+        text: text
+    };
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {
+            access_token: token
+        },
+        method: 'POST',
+        json: {
+            recipient: {
+                id: sender
+            },
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error:', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+}
