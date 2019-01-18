e5ae58792d2e4313ff46d3cc2d0cb25944cc9f20
Franklin John Zamora <jfzam@Franklins-MacBook-Pro.local>
2019-01-18 09:47:58 +0800

    installed request

--- index.js
+++ index.js
@@ -3,8 +3,7 @@
 const
   express = require('express'),
   bodyParser = require('body-parser'),
-  app = express().use(bodyParser.json()),
-  request = express.request
+  app = express().use(bodyParser.json())
 
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'))
 
