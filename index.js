37c7afbc8444ff31e184814cf07b636020920574
Franklin John Zamora <jfzam@Franklins-MacBook-Pro.local>
2019-01-21 09:50:01 +0800

    added equation function

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 'use strict'
 
 const
-  express = require('express'),
-  bodyParser = require('body-parser'),
-  app = express().use(bodyParser.json()),
-  request = require('request')
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express().use(bodyParser.json()),
+    request = require('request')
 
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'))
 
-//Home
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.get('/webhook/', (req, res) => {
-  if(req.query['hub.verify_token'] === 'jayzero_token') {
-    res.send(req.query['hub.challenge'])
-  }
-  else {
-    res.send('Wrong token!')
-  }
+    if (req.query['hub.verify_token'] === 'jayzero_token') {
+        res.send(req.query['hub.challenge'])
+    } else {
+        res.send('Wrong token!')
+    }
 })
 
 const token = "EAAMt85OjL0oBABZC69495kbvYByjsEeTPHITp81gXHJZAbg1XZBI4TRNV2i1kLfkWyvRhrQrlGs2dhniLTSWNT9FCWURqRqLB4ZB3BQAKUVT9eZAR013uZBIxhXtfHGhCV8jcxctWvkvGmXBFVXhWVajmlbIaOOEfjmd03kIa3iAZDZD";
+
 app.post('/webhook/', function(req, res) {
     var messaging_events = req.body.entry[0].messaging;
     for (var i = 0; i < messaging_events.length; i++) {
@@ -38,8 +37,38 @@ app.post('/webhook/', function(req, res) {
 })
 
 function sendTextMessage(sender, text) {
+    let equation = text
+    let res = ''
+    let unsupportedOps = ''
+
+    if (equation.replace(/[^^]/g, '').length > 0) {
+        unsupportedOps += '^, '
+    }
+    if (equation.replace(/[^%]/g, '').length > 0) {
+        unsupportedOps += '%, '
+    }
+    if (equation.replace(/[^x]/g, '').length > 0) {
+        unsupportedOps += 'x, '
+    }
+    if (equation.replace(/[^X]/g, '').length > 0) {
+        unsupportedOps += 'X, '
+    }
+    if (equation.replace(/[^=]/g, '').length > 0) {
+        unsupportedOps += '=, '
+    }
+    if (unsupportedOps.length > 0) {
+        res = `[${unsupportedOps.substr(0, unsupportedOps.length -2)}] is not supported`
+    } else {
+        let eq = equation.replace(/[^0-9+-/*()]/g, '')
+        try {
+            res = eval(eq)
+        } catch (err) {
+            res = 'syntax error'
+        }
+    }
+
     var messageData = {
-        text: text
+        text: res
     }
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
